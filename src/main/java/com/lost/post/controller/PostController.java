6616f2b95e6cb0422b844d38e1a9be8b03a73901
MJoon-Jung <gjgjajaj31@gmail.com>
2023-07-06 17:56:24 +0900

    fix: controller 어노테이션 rest controller 어노테이션으로 수정

--- src/main/java/com/lost/post/controller/PostController.java
+++ src/main/java/com/lost/post/controller/PostController.java
@@ -4,12 +4,12 @@ import com.lost.post.controller.response.PostResponse;
 import com.lost.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/posts")
 @RequiredArgsConstructor
 public class PostController {
