afd1220e1206f879a7aad97936fb4107865b9476
MJoon-Jung <gjgjajaj31@gmail.com>
2023-07-13 18:59:03 +0900

    fix: response에서 role 정보 제외

--- src/main/java/com/lost/user/controller/response/UserResponse.java
+++ src/main/java/com/lost/user/controller/response/UserResponse.java
@@ -2,7 +2,6 @@ package com.lost.user.controller.response;
 
 import com.lost.post.controller.response.PostResponse;
 import com.lost.user.domain.User;
-import com.lost.user.domain.UserRole;
 import com.querydsl.core.annotations.QueryProjection;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,7 +14,6 @@ public class UserResponse {
     private final Long id;
     private final String email;
     private final String nickname;
-    private final UserRole role;
     private final List<PostResponse> writePosts;
     private final List<PostResponse> findPosts;
     private final LocalDateTime createdAt;
@@ -23,12 +21,11 @@ public class UserResponse {
 
     @Builder
     @QueryProjection
-    public UserResponse(Long id, String email, String nickname, UserRole role, List<PostResponse> writePosts,
+    public UserResponse(Long id, String email, String nickname, List<PostResponse> writePosts,
             List<PostResponse> findPosts, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.id = id;
         this.email = email;
         this.nickname = nickname;
-        this.role = role;
         this.writePosts = writePosts;
         this.findPosts = findPosts;
         this.createdAt = createdAt;
@@ -40,7 +37,6 @@ public class UserResponse {
                 .id(user.getId())
                 .email(user.getEmail())
                 .nickname(user.getNickname())
-                .role(user.getRole())
                 .writePosts(user.getWritePosts().stream().map(PostResponse::from).toList())
                 .findPosts(user.getFindPosts().stream().map(PostResponse::from).toList())
                 .createdAt(user.getCreatedAt())
