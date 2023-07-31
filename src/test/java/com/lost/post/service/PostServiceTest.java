5a4835ae61f2dba1436803c0ed6e4bbb42662cd3
MJoon-Jung <gjgjajaj31@gmail.com>
2023-08-01 01:29:18 +0900

    fix: PostServiceTest 주석 삭제 후 테스트 수정
    기존에 코드 수정으로 인해 통과하지 못하던 테스트를 주석 삭제 후 통과하도록 수정하였습니다.
    - 존재하는 게시글을 조회하면 성공적으로 게시글을 반환하는 테스트
    - 존재하지 않는 게시글을 조회하면 예외 발생 테스트

--- src/test/java/com/lost/post/service/PostServiceTest.java
+++ src/test/java/com/lost/post/service/PostServiceTest.java
@@ -1,81 +1,83 @@
-//package com.lost.post.service;
-//
-//import static org.assertj.core.api.Assertions.assertThatThrownBy;
-//import static org.junit.jupiter.api.Assertions.assertAll;
-//
-//import com.lost.common.domain.exception.ResourceNotFoundException;
-//import com.lost.fake.TestContainer;
-//import com.lost.post.domain.Address;
-//import com.lost.post.domain.LostItem;
-//import com.lost.post.domain.PostStatus;
-//import com.lost.post.domain.TradeType;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//class PostServiceTest {
-//
-//    private PostService postService;
-//    private TestContainer testContainer;
-//
-//    @BeforeEach
-//    void setUp() {
-//        testContainer = new TestContainer();
-//        postService = new PostService(testContainer.postRepository);
-//    }
-//
-//    @Test
-//    @DisplayName("게시글 조회 성공")
-//    void should_succeed_when_find_exist_post() {
-//        //given
-//        User user = User.builder()
-//                .id(1L)
-//                .email("example@email.com")
-//                .nickname("example")
-//                .password("password")
-//                .build();
-//        testContainer.userRepository.save(user);
-//
-//        Post post = Post.builder()
-//                .title("This is post title")
-//                .content("This is post content")
-//                .tradeType(TradeType.DIRECT)
-//                .writer(user)
-//                .lostItem(LostItem.builder()
-//                        .name("airpods")
-//                        .address(Address.builder()
-//                                .street("서울시 광진구 아차산로")
-//                                .latitude(37.123)
-//                                .longitude(127.456)
-//                                .build())
-//                        .build())
-//                .reward(10_000)
-//                .status(PostStatus.PENDING)
-//                .build();
-//        post = testContainer.postRepository.save(post);
-//        //when
-//        Post findPost = postService.postDetails(post.getId());
-//        //then
-//        assertAll(
-//                () -> assertThat(findPost.getTitle()).isEqualTo("This is post title"),
-//                () -> assertThat(findPost.getContent()).isEqualTo("This is post content"),
-//                () -> assertThat(findPost.getTradeType()).isEqualTo(TradeType.DIRECT),
-//                () -> assertThat(findPost.getTradeType()).isEqualTo(TradeType.DIRECT),
-//                () -> assertThat(findPost.getStatus()).isEqualTo(PostStatus.PENDING),
-//                () -> assertThat(findPost.getLostItem().getName()).isEqualTo("airpods"),
-//                () -> assertThat(findPost.getLostItem().getAddress().getLatitude()).isEqualTo(37.123),
-//                () -> assertThat(findPost.getLostItem().getAddress().getLongitude()).isEqualTo(127.456),
-//                () -> assertThat(findPost.getLostItem().getAddress().getStreet()).isEqualTo("서울시 광진구 아차산로")
-//        );
-//    }
-//
-//    @Test
-//    @DisplayName("게시글 생성 실패 - 존재하지 않는 게시글")
-//    void should_fail_when_find_non_existent_post() {
-//        //given
-//        //when
-//        //then
-//        assertThatThrownBy(() -> postService.postDetails(1L))
-//                .isInstanceOf(ResourceNotFoundException.class);
-//    }
-//}
+package com.lost.post.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.lost.common.domain.exception.ResourceNotFoundException;
+import com.lost.post.domain.Address;
+import com.lost.post.domain.JpaAddress;
+import com.lost.post.domain.Post;
+import com.lost.post.domain.PostStatus;
+import com.lost.post.domain.TradeType;
+import com.lost.post.infra.repository.PostRepository;
+import com.lost.user.domain.User;
+import com.lost.user.service.repostiory.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @DisplayName("존재하는 게시글을 조회하면 성공적으로 게시글을 반환한다.")
+    void postService_load_return_exist_post() {
+        //given
+        User user = User.builder()
+                .id(1L)
+                .email("example@email.com")
+                .nickname("example")
+                .password("password")
+                .build();
+        userRepository.save(user);
+
+        Post post = Post.builder()
+                .content("This is post content")
+                .tradeType(TradeType.DIRECT)
+                .user(user)
+                .itemName("airpods")
+                .reward(10_000)
+                .status(PostStatus.PENDING)
+                .address(JpaAddress.from(Address.builder()
+                        .street("서울시 광진구 아차산로")
+                        .latitude(37.123)
+                        .longitude(127.456)
+                        .build()))
+                .build();
+        post = postRepository.save(post);
+        //when
+        Post findPost = postService.load(post.getId());
+        //then
+        assertAll(
+                () -> assertThat(findPost.getContent()).isEqualTo("This is post content"),
+                () -> assertThat(findPost.getTradeType()).isEqualTo(TradeType.DIRECT),
+                () -> assertThat(findPost.getTradeType()).isEqualTo(TradeType.DIRECT),
+                () -> assertThat(findPost.getStatus()).isEqualTo(PostStatus.PENDING),
+                () -> assertThat(findPost.getItemName()).isEqualTo("airpods"),
+                () -> assertThat(findPost.getAddress().toModel().getLatitude()).isEqualTo(37.123),
+                () -> assertThat(findPost.getAddress().toModel().getLongitude()).isEqualTo(127.456),
+                () -> assertThat(findPost.getAddress().toModel().getStreet()).isEqualTo("서울시 광진구 아차산로")
+        );
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 게시글을 조회 요청하면 예외가 발생한다.")
+    void should_fail_when_find_non_existent_post() {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> postService.load(1L))
+                .isInstanceOf(ResourceNotFoundException.class);
+    }
+}
